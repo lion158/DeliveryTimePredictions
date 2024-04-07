d3108aa46e9ca0dfd1f91835b6e2431b2de4ac27
lion158 <szczepandpolak@gmail.com>
2024-04-07 21:50:35 +0200

    analysis

--- queries.sql
+++ queries.sql
@@ -9,14 +9,31 @@ GROUP BY op.product_id
 ORDER BY totalWeight ASC
 
 # Part 2
+
+# route segments data
 SELECT *, TIMESTAMPDIFF(SECOND, segment_start_time, segment_end_time) AS time_diff_in_seconds FROM route_segments
-ORDER BY driver_id, segment_id DESC, segment_end_time ASC
+ORDER BY driver_id, segment_id DESC, segment_end_time ASC;
 # WHERE driver_id = 1
 # ORDER BY segment_start_time ASC, segment_end_time DESC, segment_id DESC
 # ORDER BY segment_start_time ASC
 # GROUP BY order_id
 
+# orders planed delivery data
+SELECT order_id, planned_delivery_duration, sector_id FROM orders
 
 
-SELECT * FROM route_segments
-order by driver_id, segment_id, segment_end_time
+# weight of every order
+SELECT o.order_id, SUM(p.weight * op.quantity) AS total_weight
+FROM orders o
+JOIN orders_products op ON o.order_id = op.order_id
+JOIN products p ON op.product_id = p.product_id
+GROUP BY o.order_id;
+
+# order quantity
+SELECT o.order_id, SUM(op.quantity) AS total_quantity
+FROM orders o
+JOIN orders_products op ON o.order_id = op.order_id
+GROUP BY o.order_id;
+
+
+
